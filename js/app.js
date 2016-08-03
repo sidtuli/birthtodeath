7172933a1f802a4d4801258cb6904804172ab298
sidtuli <stuli@terpmail.umd.edu>
2016-08-02 23:19:31 -0400

    Minor setup for google maps API integration.

--- js/app.js
+++ js/app.js
@@ -315,3 +315,32 @@ bdApp.service('parseService',function(){
     };
 });
 
+bdApp.service('mapService', function(){
+    var directionsService = new google.maps.DirectionsService;
+    var directionsDisplay = new google.maps.DirectionsRenderer;
+    this.init = function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 7,
+          center: {lat: 41.85, lng: -87.65}
+        });
+        directionsDisplay.setMap(map);
+        this.calculateAndDisplayRoute(directionsService, directionsDisplay, "Chicago", "Washington D.C.");
+        
+        
+      };
+    this.calculateAndDisplayRoute = function(originString, destinationString) {
+        directionsService.route({
+          origin: originString,
+          destination: destinationString,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            console.log(response);
+          } else {
+            window.alert('Directions request failed due to ' + status);
+          }
+        });
+      };
+});
+
