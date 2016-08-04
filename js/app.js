cb299da7e2baa843a799e3fb032f86eccb0571cc
sidtuli <stuli@terpmail.umd.edu>
2016-08-03 23:14:00 -0400

    Attempting to bring ng-map angular google maps plugin.

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ bdApp.controller('bdController',['$scope','apiService','checkService','parseServ
     $scope.log = function(message) {
         console.log(message);
     };
+    
     // A variable to track which state we are currently in
     // phase[0] - a person's info
     // phase[1] - a 'may refer to' list
@@ -315,32 +316,6 @@ bdApp.service('parseService',function(){
     };
 });
 
-bdApp.service('mapService', function(){
-    var directionsService = new google.maps.DirectionsService;
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    this.init = function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 7,
-          center: {lat: 41.85, lng: -87.65}
-        });
-        directionsDisplay.setMap(map);
-        this.calculateAndDisplayRoute(directionsService, directionsDisplay, "Chicago", "Washington D.C.");
-        
-        
-      };
-    this.calculateAndDisplayRoute = function(originString, destinationString) {
-        directionsService.route({
-          origin: originString,
-          destination: destinationString,
-          travelMode: 'DRIVING'
-        }, function(response, status) {
-          if (status === 'OK') {
-            directionsDisplay.setDirections(response);
-            console.log(response);
-          } else {
-            window.alert('Directions request failed due to ' + status);
-          }
-        });
-      };
-});
+
+
 
--- js/app2.js
+++ js/app2.js
@@ -1,5 +1,13 @@
-var bdApp = angular.module('bdApp',[]);
+var testApp = angular.module('testApp',['uiGmapgoogle-maps']);
 
-bdApp.controller('bdController',['$scope',function($scope){
+testApp.controller('testCtrlr',['$scope',function($scope){
     
 }]);
+
+testApp.config(function(uiGmapGoogleMapApiProvider) {
+    uiGmapGoogleMapApiProvider.configure({
+        key: 'AIzaSyATLVXUzJhvTD-xV96EpM1B4bBnWYGhEPI',
+        v: '3.20', //defaults to latest 3.X anyhow
+        libraries: 'weather,geometry,visualization'
+    });
+})
