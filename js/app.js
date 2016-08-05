df43c9dffd414f0a6c2aae5f18b21e44179f9593
sidtuli <stuli@terpmail.umd.edu>
2016-08-04 23:09:18 -0400

    going to need to learn how to better use ng-map?

--- js/app.js
+++ js/app.js
@@ -53,7 +53,7 @@ bdApp.controller('bdController',['$scope','apiService','checkService','parseServ
                     $scope.age = deathDate.hasOwnProperty("invalid") || birthDate.hasOwnProperty("invalid") ? "Wikipedia did not supply correct info to find age" : parseService.formateAge(deathDate.dateNum,birthDate.dateNum);
                     
                     $scope.phase = [true,false];
-                    console.log(NgMap.initMap());
+                    console.log(NgMap.getMap("map"));
                     
                 }
                 
